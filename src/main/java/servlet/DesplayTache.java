462c17579ac19663eabaa5adb85b2d68e1d235d8
stouf <ouadad.mustapha01@gmail.com>
2025-03-20 10:47:30 +0000

    creer servlet DesplayTache

---
+++ src/main/java/servlet/DesplayTache.java
@@ -0,0 +1,18 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/DesplayTache")
+public class DesplayTache extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+
+}
