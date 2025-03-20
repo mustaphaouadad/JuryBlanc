ea6f89bb7a6d5aa6e4214711533727665bced7f4
stouf <ouadad.mustapha01@gmail.com>
2025-03-20 14:38:53 +0000

    ajouter servlet

---
+++ src/main/java/servlet/EditTache.java
@@ -0,0 +1,56 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import dao.TacheDao;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Tache;
+
+
+@WebServlet("/EditTache")
+public class EditTache extends HttpServlet {
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int idTache=Integer.parseInt(request.getParameter("idTache"));
+		
+		Tache t= TacheDao.getTachebyId(idTache);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("");
+		request.setAttribute("t", t);
+		dispatcher.forward(request, response);
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int idTache = Integer.parseInt(request.getParameter("idTache"));
+		String descriptionTache=request.getParameter("descriptionTache");
+		String dateDebutTache=request.getParameter("dateDebutTache");
+		String dateFinTache=request.getParameter("dateFinTache");
+		
+		Tache t=new Tache();
+		t.setIdTache(idTache);
+		t.setDescriptionTache(descriptionTache);
+		t.setDateDebutTache(dateDebutTache);
+		t.setDateFinTache(dateFinTache);
+		int result=TacheDao.updateTache(t);
+		
+		if (result>0) {
+        	response.sendRedirect("DesplayTache?idProjet="+ idTache);
+			
+		} else {
+			PrintWriter out=response.getWriter();
+			out.println("Eroor while updating");
+
+		}
+		
+		
+	}
+
+}
