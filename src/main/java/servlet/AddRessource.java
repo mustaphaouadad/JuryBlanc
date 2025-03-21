c80a727c9077463720781dfec62b359c9635e9f4
stouf <ouadad.mustapha01@gmail.com>
2025-03-21 11:41:53 +0000

    creer servlet AddRessource

---
+++ src/main/java/servlet/AddRessource.java
@@ -0,0 +1,42 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import dao.RessourcesDao;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Ressources;
+
+
+@WebServlet("/AddRessource")
+public class AddRessource extends HttpServlet {
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		 String nom = request.getParameter("nomRessource");
+	        String type = request.getParameter("typeRessource");
+	        int quantite = Integer.parseInt(request.getParameter("quantite"));
+	        
+	        Ressources r=new Ressources();
+	        r.setNomRessource(nom);
+	        r.setTypeRessource(type);
+	        r.setQuantite(quantite);
+	        int result=RessourcesDao.addRessource(r);
+	        
+	        if (result>0) {
+				response.sendRedirect("DesplayProjet");
+				
+			} else {
+				PrintWriter out = response.getWriter();
+				out.println("Data not add ");
+
+			}
+		
+		
+	}
+
+}
